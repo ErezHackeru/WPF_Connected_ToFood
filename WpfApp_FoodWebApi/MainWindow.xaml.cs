5446afad9ab499d3888d564cfdd102ab48754ddd
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-09-19 15:55:58 +0300

    Add files via upload

---
+++ WpfApp_FoodWebApi/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_FoodWebApi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> listOfFoods = new ObservableCollection<string>();
+        private const string URL = "http://localhost:64047/api/food/search?name=p";
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListbxGet.DataContext = listOfFoods;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // -------------------------- Get One item
+            HttpResponseMessage response = client.GetAsync("").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                var dataObjects = response.Content.ReadAsStringAsync().Result;
+                ListbxGet.Items.Add(dataObjects);
+                               
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            
+        }
+    }
+}
